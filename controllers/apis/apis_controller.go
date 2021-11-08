64e6a8f4850da87544a368afbc11720e3f64a4f5
Katsumasa-Sato <katsumasa-sato@outlook.com>
2021-11-08 09:40:59 +0900

    apis_controllerにerrorを追加

--- controllers/apis/apis_controller.go
+++ controllers/apis/apis_controller.go
@@ -1,18 +1,25 @@
 package apis
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/katsun0921/portfolio_api/domain/apis"
-  "github.com/katsun0921/portfolio_api/services"
-  "net/http"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/katsun0921/go_utils/logger"
+	"github.com/katsun0921/go_utils/rest_errors"
+	"github.com/katsun0921/portfolio_api/domain/apis"
+	"github.com/katsun0921/portfolio_api/services"
+	"net/http"
 )
 
 func Get(c *gin.Context) {
-  var api apis.Api
+	var api apis.Api
 
-  rss, err := services.ApisService.GetApi(api)
-  if err != nil {
-    return
-  }
-  c.JSON(http.StatusOK, rss)
+	rss, err := services.ApisService.GetApi(api)
+	if err != nil {
+		logger.Error("error when trying to api request", err)
+		restErr := rest_errors.NewBadRequestError("invalid json error", errors.New("json error"))
+		if restErr != nil {
+		  return
+    }
+	}
+	c.JSON(http.StatusOK, rss)
 }

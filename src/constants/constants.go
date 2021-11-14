0cfc630cf555bd78f53dac822fe1ce53f2221107
Katsumasa-Sato <katsumasa-sato@outlook.com>
2021-11-14 18:01:34 +0900

    create constants files

---
+++ src/constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+  ZENN = "zenn"
+  TWITTER = "twitter"
+  DateLayout = "2006/01/02 15:00"
+)
--- src/domain/apis/api_dto.go
+++ src/domain/apis/api_dto.go
@@ -2,7 +2,7 @@ package apis
 
 type Api struct {
 	Id          int    `json:"id"`
-	Text   string `json:"text"`
+	Text        string `json:"text"`
 	Link        string `json:"link"`
 	Service     string `json:"service"`
 	DateCreated string `json:"date_created"`

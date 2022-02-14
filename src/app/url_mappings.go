6884cf226b15982a6b6805d39b3c28418ddc8045
Katsumasa-Sato <katsumasa-sato@outlook.com>
2022-02-15 07:06:33 +0900

    Comment out articles

--- src/app/url_mappings.go
+++ src/app/url_mappings.go
@@ -2,13 +2,13 @@ package app
 
 import (
   "github.com/katsun0921/portfolio_api/src/controllers/apis"
-  "github.com/katsun0921/portfolio_api/src/controllers/articles"
   "github.com/katsun0921/portfolio_api/src/controllers/ping"
 )
 
 func mapUrls() {
   router.GET("/ping", ping.Ping)
   router.GET("/apis", apis.Get)
-  router.GET("/articles", articles.Get)
-  router.POST("/articles", articles.Create)
+  //:TODO Comment out until post is made.
+  // router.GET("/articles", articles.Get)
+  // router.POST("/articles", articles.Create)
 }

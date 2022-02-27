32e4db820436449d84a8dee66816f1bf63ddd15b
Katsumasa-Sato <katsumasa-sato@outlook.com>
2022-02-27 16:31:37 +0900

    Add CORS config

--- src/app/application.go
+++ src/app/application.go
@@ -10,8 +10,29 @@ var (
 )
 
 func StartApplication() {
+	router.Use(CORSMiddleware())
+
 	mapUrls()
 
 	logger.Info("about to start the application...")
 	router.Run(":3000")
+}
+
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
